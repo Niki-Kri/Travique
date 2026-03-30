c562e49c78403277c7b5a795c974cd45f4aa6130
Niki Kumari <2024ugcs073@gmail.com>
2026-03-31 02:05:07 +0530

    updated cloudinay config and fixes

--- cloudConfig.js
+++ cloudConfig.js
@@ -3,15 +3,15 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
     cloud_name:process.env.CLOUD_NAME,
-    api_key:process.env.CLOUD_APT_KEY,
-   api_secret:process.env.CLOUD_APT_SECRET
+    api_key:process.env.CLOUD_API_KEY,
+   api_secret:process.env.CLOUD_API_SECRET
 });
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: 'travique_DEV',
-    allowedFormats: ["png","jpg","jpeg"],
+    allowedFormats: ["png","jpg","jpeg","avif"],
   },
 });
 
